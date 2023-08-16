91d43f67cc9ff79af911fd2463da9bde4d44661a
lukaross368 <luka.rossapollo@gmail.com>
2023-08-16 17:06:51 +0100

    added my first app

--- src/firstDjagno/settings.py
+++ src/firstDjagno/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third party apps
+
+    # your apps
+
+    'products',
 ]
 
 MIDDLEWARE = [
