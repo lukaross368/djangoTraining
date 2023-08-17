c7c157fee228251a1319a798c24dd31f66da8a75
lukaross368 <luka.rossapollo@gmail.com>
2023-08-17 11:25:31 +0100

    added pages and views

--- src/firstDjagno/settings.py
+++ src/firstDjagno/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -41,8 +41,8 @@ INSTALLED_APPS = [
     # third party apps
 
     # your apps
-
     'products',
+    'pages',
 ]
 
 MIDDLEWARE = [
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'firstDjagno.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ src/pages/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home_view(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
+    return render(request, "home.html", {})
+
+def contact_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Contact Page</h1>")
+
+def about_view(request, *args, **kwargs):
+    return HttpResponse("<h1>About Page</h1>")
+
+def social_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Social Page</h1>")
